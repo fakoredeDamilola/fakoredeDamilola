327ab6ed1fbe52a419744fe0dcf82654eae55957
Fakorede Damilola <dfakorede29@gmail.com>
2020-12-07 22:40:28 +0100

    uploading my portfolio website

---
+++ script.js
@@ -0,0 +1,34 @@
+// define variables and dom elements
+let menuToggle = document.querySelector(".menu-toggle")
+let closeNav = document.querySelector(".close-nav")
+let menu = document.querySelector(".menu")
+let mouseDown = document.querySelector(".mouse-down")
+
+let links = document.querySelectorAll(".menu ul li a")
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+
+// define eventlisteners
+menuToggle.addEventListener("click", function () {
+    menu.classList.add("active")
+
+})
+closeNav.addEventListener("click", function () {
+    menu.classList.remove("active")
+
+})
+mouseDown.addEventListener("click", clickHandler)
+
+function clickHandler(e) {
+    e.preventDefault();
+    menu.classList.remove("active");
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
+}
